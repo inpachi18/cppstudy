6324d114c50015b69317999fce5beebeb53aaff1
Stake18 <swjh2000@foxmail.com>
2020-12-20 17:41:31 +0800

    update

---
+++ Project 7-2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main ()
+{
+int N,M;
+scanf ("%d %d",&N,&M);
+
+
+}
---
+++ test.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void) {
+    float X = 0x414C0000;
+    printf("%f",X);
+}
